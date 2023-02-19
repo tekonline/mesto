03cfb2da8c208fe6736e89983e19f0baf1feee16
Erik <teconline@yandex.ru>
2023-02-19 17:03:40 +0700

    feat: закончить JS

--- script.js
+++ script.js
@@ -1,2 +1,34 @@
-let editButton = document.querySelector(".profile__edit-button");
-editButton.addEventListener("onclick", "")
+let popup = document.querySelector (".popup")
+let editButton = document.querySelector (".profile__edit-button")
+let closeButton = document.querySelector (".popup__close-btn")
+
+let formElement = document.querySelector (".popup__content")
+let inputName = document.querySelector (".popup__input-name")
+let inputJob = document.querySelector (".popup__input-job")
+let profileTitle = document.querySelector (".profile__title")
+let profileJob = document.querySelector (".profile__subtitle")
+let saveButton = document.querySelector (".popup__save")
+
+
+function openPopup () {
+    popup.classList.add("popup__opened")
+    inputName.value = profileTitle.textContent;
+    inputJob.value = profileJob.textContent;
+}
+
+function closePopup () {
+    popup.classList.remove("popup__opened")
+}
+
+function handleFormSubmit (evt) {
+    evt.preventDefault();
+    profileTitle.textContent = inputName.value;
+    profileJob.textContent = inputJob.value;
+    closePopup()
+}
+
+
+
+closeButton.addEventListener ("click", closePopup)
+editButton.addEventListener ("click", openPopup)
+formElement.addEventListener("submit", handleFormSubmit)
